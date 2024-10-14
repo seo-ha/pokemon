b7c5ca0adfe4ae56e93855e6f34d68faa4bb6531
stay-ha <PRO.git>
2024-10-14 15:38:53 +0900

    fetch 속도 개선

--- src/fetchs/pokemonFetch.js
+++ src/fetchs/pokemonFetch.js
@@ -99,39 +99,3 @@ export const pokemonDetailFetch = ({ id, setPokemonDetailData}) => {
     
     fetchData()
 }
-
-
-export const pokemonAllFetch = ({setSerchPokemonData}) => {
- 
-    const fetchData = async()=>{
-        const allPokemonData = [];
-        
-        for (let i = 1; i <= totalPokemon ; i++) {
-            
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-            const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
-            const koreanName = speciesResponse.data.names.find(name => name.language.name === 'ko');
-            
-            const typesWithKoreanNames = await Promise.all(
-                response.data.types.map(async (type) => {
-                    const typeResponse = await axios.get(type.type.url);
-                    const koreanTypeName = typeResponse.data.names.find(
-                        (name) => name.language.name === 'ko'
-                    ).name;
-                    return { ...type, type: { ...type.type, korean_name: koreanTypeName } };
-                })
-            );
-            
-            //최종 데이터
-            allPokemonData.push({ 
-                id : response.data.id,
-                img : response.data.sprites.other["official-artwork"].front_default,
-                korean_name: koreanName.name, 
-                type : typesWithKoreanNames,
-            });
-            
-        }
-        setSerchPokemonData(allPokemonData);
-    };
-    fetchData()
-}
