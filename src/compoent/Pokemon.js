fe018139bd1bca74ce665013ff0830c05e7595e4
seo-ha <mk096@naver.com>
2025-01-23 21:21:28 +0900

    반응형 수정

--- src/compoent/Pokemon.js
+++ src/compoent/Pokemon.js
@@ -1,156 +1,198 @@
-
 import React, { useEffect, useMemo, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import PokemonDetail from './PokemonDetail';
-import logo from '../assets/logo.png'
-import pokeball from '../assets/pokeball.png';
-import { pokemonFetch, pokemonDetailFetch, pokemonAllFetch } from "../fetchs/pokemonFetch";
+import PokemonDetail from "./PokemonDetail";
+import logo from "../assets/logo.png";
+import pokeball from "../assets/pokeball.png";
+import {
+  pokemonFetch,
+  pokemonDetailFetch,
+  pokemonAllFetch,
+} from "../fetchs/pokemonFetch";
 // import { pokemonAllFetch } from "../fetchs/pokemonAllFetch";
 
 const Pokemon = () => {
-   
-    const [pokemonData, setPokemonData] = useState([])
-    const [searchPokemonData, setSerchPokemonData] = useState([])
-    const [pokemonDetailData, setPokemonDetailData] = useState([]);
-    const [searchList, setSearchList] = useState();
-    const [currentpage, setCurrentpage] = useState(1);
-    const [searchPokemonLenght, setSearchPokemonLenght] = useState(1);
-    const [searchInput, setSearchInput] = useState('');
-    const view = document.querySelector('.pokemonDetail');
-    const pokemonPerPage = 20;
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchPokemonData, setSerchPokemonData] = useState([]);
+  const [pokemonDetailData, setPokemonDetailData] = useState([]);
+  const [searchList, setSearchList] = useState();
+  const [currentpage, setCurrentpage] = useState(1);
+  const [searchPokemonLenght, setSearchPokemonLenght] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+  const view = document.querySelector(".pokemonDetail");
+  const pokemonPerPage = 20;
 
-    
-    //포켓몬 api 받아오기
-     
-    useEffect(() => {
+  //포켓몬 api 받아오기
 
-        pokemonFetch({setPokemonData, currentpage, pokemonPerPage});
-        return () =>{}
+  useEffect(() => {
+    pokemonFetch({ setPokemonData, currentpage, pokemonPerPage });
+    return () => {};
+  }, [currentpage]);
 
-    },[currentpage])
-    
-    useMemo(()=>{
-         if(pokemonData.length === 20) {
-            pokemonAllFetch(setSerchPokemonData)
-        }
-    },[pokemonData.length])
-
-    
-    //스크롤을 내렸을때 페이지 추가하기
-    const fetchMoerData = ()=> {
-        setCurrentpage((prevPage) => prevPage + 1);
-    };
-    
-    //포켓몬 디테일 페이지 
-    const onPokemonDataliData = (idx, id, name, type)=> {
-        setPokemonDetailData([])
-        
-        pokemonDetailFetch({id, name, type, setPokemonDetailData});
-        
-        var items = document.querySelectorAll('.listUl .item');
-        const detailTxts = document.querySelectorAll('.light');
-
-        for (let i = 0; i < items.length; i++) {
-            items[i].classList.remove('on');
-        };
-        
-        for (let i = 0; i < detailTxts.length; i++) {
-            detailTxts[i].classList.remove('on')
-        }
-        
-        idx.parentNode.classList.add('on');
-        view.classList.add('on');
-        
-    };
-    
-    //모바일에서 디테일 페이지 닫기 버튼
-    const onCloseView = ()=> {
-        view.classList.remove('on');
-    };
-
-    //포켓몬 검색
-    const search = (e) =>{
-    
-        setSearchInput(e.target.value);
-        
-        if(searchInput.length >= 0) {
-            var searchDataList = searchPokemonData.filter((item)=> item.korean_name.includes(e.target.value))
-            
-            setSearchList(searchDataList);
-            setSearchPokemonLenght(searchDataList.length);
-        }
+  useMemo(() => {
+    if (pokemonData.length === 20) {
+      pokemonAllFetch(setSerchPokemonData);
     }
-    
-    return (
-        <div className="content">
-            
-            <input className={searchPokemonData.length > 0 ? 'on' : ''} type="search" name="" id="search" placeholder={searchPokemonData.length > 0 ? 'search...' : 'loading..'} value={searchInput} onChange={(e)=>search(e)} disabled={searchPokemonData.length > 0 ? false : true}/>
-          
-            <div className="pokemonList">
-             
-                <InfiniteScroll
-                    height={'100%'}
-                    dataLength={searchInput.length <= 0 ? pokemonData.length : searchPokemonLenght.length}
-                    next={searchInput.length <= 0 ? fetchMoerData : ''}
-                    hasMore={searchInput.length <= 0 ? currentpage * pokemonPerPage < 151 : ''}
-                    loader={<div className="loading"><img src={pokeball} alt="loading"/></div>}
-                    className="container"
-                >
-                    <ul className="listUl">
-                        {
-                            searchInput.length <= 0 
-                            ? pokemonData.map((item)=>{
-                                
-                            return <li key={item.id} className="item">
-                                    <button onClick={(e)=> onPokemonDataliData(e.target, item.id, item.korean_name, item.type) }>
-                                        <img src={item.img} alt={item.korean_name} loading="lazy"/>
-                                        <p>{item.korean_name}</p>
-                                        <div className="types">
-                                            {
-                                                item.type.map(({type}, idx)=>{
-                                                    return <span key={idx} className={type.name}>
-                                                                {type.korean_name}
-                                                            </span>
-                                                })
-                                            }
-                                        </div>
-                                    </button>
-                                </li>
-                            })
-                            : searchList.map((item)=>{
-                            return <li key={item.id} className="item">
-                                    <button onClick={(e)=> onPokemonDataliData(e.target, item.id, item.korean_name, item.type) }>
-                                        <img src={item.img} alt={item.korean_name} loading="lazy"/>
-                                        <p>{item.korean_name}</p>
-                                        <div className="types">
-                                            {
-                                                item.type.map(({type}, idx) => {
-                                                    return <span key={idx} className={type.name}>
-                                                                {type.korean_name}
-                                                            </span>
-                                                })
-                                            }
-                                        </div>
-                                    </button>
-                                </li>
-                            })
+  }, [pokemonData.length]);
+
+  //스크롤을 내렸을때 페이지 추가하기
+  const fetchMoerData = () => {
+    setCurrentpage((prevPage) => prevPage + 1);
+  };
+
+  //포켓몬 디테일 페이지
+  const onPokemonDataliData = (idx, id, name, type) => {
+    setPokemonDetailData([]);
+
+    pokemonDetailFetch({ id, name, type, setPokemonDetailData });
+
+    var items = document.querySelectorAll(".listUl .item");
+    const detailTxts = document.querySelectorAll(".light");
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove("on");
+    }
+
+    for (let i = 0; i < detailTxts.length; i++) {
+      detailTxts[i].classList.remove("on");
+    }
+
+    idx.parentNode.classList.add("on");
+    view.classList.add("on");
+  };
+
+  //모바일에서 디테일 페이지 닫기 버튼
+  const onCloseView = () => {
+    view.classList.remove("on");
+  };
+
+  //포켓몬 검색
+  const search = (e) => {
+    setSearchInput(e.target.value);
+
+    if (searchInput.length >= 0) {
+      var searchDataList = searchPokemonData.filter((item) =>
+        item.korean_name.includes(e.target.value)
+      );
+
+      setSearchList(searchDataList);
+      setSearchPokemonLenght(searchDataList.length);
+    }
+  };
+
+  return (
+    <div className="content">
+      <input
+        className={searchPokemonData.length > 0 ? "on" : ""}
+        type="search"
+        name=""
+        id="search"
+        placeholder={searchPokemonData.length > 0 ? "search..." : "loading.."}
+        value={searchInput}
+        onChange={(e) => search(e)}
+        disabled={searchPokemonData.length > 0 ? false : true}
+      />
+
+      <div className="pokemonList">
+        <InfiniteScroll
+          height={"100%"}
+          dataLength={
+            searchInput.length <= 0
+              ? pokemonData.length
+              : searchPokemonLenght.length
+          }
+          next={searchInput.length <= 0 ? fetchMoerData : ""}
+          hasMore={
+            searchInput.length <= 0 ? currentpage * pokemonPerPage < 151 : ""
+          }
+          loader={
+            <div className="loading">
+              <img src={pokeball} alt="loading" />
+            </div>
+          }
+          className="container"
+        >
+          <ul className="listUl">
+            {searchInput.length <= 0
+              ? pokemonData.map((item) => {
+                  return (
+                    <li key={item.id} className="item">
+                      <button
+                        onClick={(e) =>
+                          onPokemonDataliData(
+                            e.target,
+                            item.id,
+                            item.korean_name,
+                            item.type
+                          )
                         }
-                    </ul>
-                </InfiniteScroll>
-                      
-            </div>
-            
-            <div className="pokemonDetail">
-                <button className="closeView" onClick={()=>onCloseView()}></button>
-                {
-                    pokemonDetailData.length === 0
-                    ? <div className="listDefault"><img src={logo} alt="포켓몬 로고" className='logo' /></div>
-                    : <PokemonDetail pokemon={pokemonDetailData[0]}/>
-                }
-            </div>
-            
-        </div>
-    )
-}
+                      >
+                        <img
+                          src={item.img}
+                          alt={item.korean_name}
+                          loading="lazy"
+                        />
+                        <p>{item.korean_name}</p>
+                        <div className="types">
+                          {item.type.map(({ type }, idx) => {
+                            return (
+                              <span key={idx} className={type.name}>
+                                {type.korean_name}
+                              </span>
+                            );
+                          })}
+                        </div>
+                      </button>
+                    </li>
+                  );
+                })
+              : searchList.map((item) => {
+                  return (
+                    <li key={item.id} className="item">
+                      <button
+                        onClick={(e) =>
+                          onPokemonDataliData(
+                            e.target,
+                            item.id,
+                            item.korean_name,
+                            item.type
+                          )
+                        }
+                      >
+                        <img
+                          src={item.img}
+                          alt={item.korean_name}
+                          loading="lazy"
+                        />
+                        <p>{item.korean_name}</p>
+                        <div className="types">
+                          {item.type.map(({ type }, idx) => {
+                            return (
+                              <span key={idx} className={type.name}>
+                                {type.korean_name}
+                              </span>
+                            );
+                          })}
+                        </div>
+                      </button>
+                    </li>
+                  );
+                })}
+          </ul>
+        </InfiniteScroll>
+      </div>
+
+      <div className="pokemonDetail">
+        <button className="closeView" onClick={() => onCloseView()}></button>
+        {pokemonDetailData.length === 0 ? (
+          <div className="listDefault">
+            <img src={logo} alt="포켓몬 로고" className="logo" />
+          </div>
+        ) : (
+          <PokemonDetail pokemon={pokemonDetailData[0]} />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default React.memo(Pokemon);
--- src/compoent/PokemonDetail.js
+++ src/compoent/PokemonDetail.js
@@ -1,140 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PokemonDetail({pokemon}) {
+function PokemonDetail({ pokemon }) {
+  const [moveIdx, setMoveIdx] = useState(0);
+  const viewHight = document.querySelector(".viewBox");
 
-  let moveIdx = 0;
-  
-  const moveTop = () => {
-    
-    let txts = document.querySelectorAll('.light');
-    if (moveIdx > txts.length ) {
-      moveIdx = 0;
-    }
-    for (let i = 0; i < txts.length; i++) {
-      
-      if(i === moveIdx) {
-        txts[i].classList.add('on')
-      } else {
-        txts[i].classList.remove('on')
-      }
-      
-    }
-    moveIdx ++ ;
-    
-  }
-  
   const moveBottom = () => {
-    let txts = document.querySelectorAll('.light');
-    
-    if (moveIdx < 0) {
-      moveIdx = txts.length - 1;
+    setMoveIdx((prevIdx) => (prevIdx + 1) % pokemon.move.length);
+
+    if (moveIdx === 3) {
+      viewHight.scrollTo({ top: viewHight.scrollHeight, behavior: "smooth" });
+    } else if (moveIdx === 4) {
+      viewHight.scrollTo({ top: 0, behavior: "smooth" });
     }
-    for (let i = 0; i < txts.length; i++) {
-      
-      if(i === moveIdx) {
-        txts[i]?.classList.add('on')
-      } else {
-        txts[i]?.classList.remove('on')
-      }
+  };
+
+  const moveTop = () => {
+    setMoveIdx((prevIdx) => (prevIdx - 1 + pokemon.move.length) % pokemon.move.length);
+
+    if (moveIdx === 0) {
+      viewHight.scrollTo({ top: viewHight.scrollHeight, behavior: "smooth" });
+    } else if (moveIdx === 1) {
+      viewHight.scrollTo({ top: 0, behavior: "smooth" });
     }
-    
-    moveIdx -- ;
-  }
-  
+  };
+
   return (
     <div className="detailBox">
-      
       <div className="viewBox">
         <div className="imgBox">
-          <img src={pokemon.img} alt={pokemon.korean_name}  loading="lazy"/>
+          <img src={pokemon.img} alt={pokemon.korean_name} loading="lazy" />
         </div>
         <div className="txtBox">
-        
-          <p className="name light">이름 : {pokemon.korean_name} [ { pokemon.genera } ]</p>
-        
-          <div className="types marginTop light">
+          <p className={moveIdx === 0 ? "name item on" : "name item"}>
+            이름 : {pokemon.korean_name} [ {pokemon.genera} ]
+          </p>
+
+          <div className={moveIdx === 1 ? "types item on" : "types item"}>
             <span>속성 : </span>
-            {
-                pokemon.type.map(({type}, idx)=>{
-                    return <span key={idx}>
-                                {type.korean_name}
-                                {idx < pokemon.type.length - 1 ? ', ' : ''}
-                            </span>
-                })
-            }
+            {pokemon.type.map(({ type }, idx) => {
+              return (
+                <span key={idx}>
+                  {type.korean_name}
+                  {idx < pokemon.type.length - 1 ? ", " : ""}
+                </span>
+              );
+            })}
           </div>
-        
-          <div className="ability marginTop light">
+
+          <div className={moveIdx === 2 ? "ability item on" : "ability item"}>
             <span>특성 : </span>
-            {
-                pokemon.abilities.map((ability, idx)=>{
-                    return <span key={idx}>
-                                {ability.korean_name}
-                                {idx < pokemon.abilities.length - 1 ? ', ' : ''}
-                            </span>
-                })
-            }
+            {pokemon.abilities.map((ability, idx) => {
+              return (
+                <span key={idx}>
+                  {ability.korean_name}
+                  {idx < pokemon.abilities.length - 1 ? ", " : ""}
+                </span>
+              );
+            })}
           </div>
-        
-          <p className="name marginTop light">특징 : {pokemon.flavor}</p>
-        
-          <div className="moves marginTop light">
+
+          <p className={moveIdx === 3 ? "name item on" : "name item"}>특징 : {pokemon.flavor}</p>
+
+          <div className={moveIdx === 4 ? "moves item on" : "moves item"}>
             <span>기술 : </span>
-            {
-                pokemon.move.map((move, idx)=>{
-                  return <span key={idx}>
-                              {
-                                move.korean_name === undefined
-                                ? ''
-                                :  move.korean_name.name
-                                }
-                                {idx < 4 ? ', ' : ''}
-                          </span>
-                })
-            }
+            {pokemon.move.map((move, idx) => {
+              return (
+                <span key={idx}>
+                  {move.korean_name && move.korean_name.name ? move.korean_name.name : ""}
+                  {idx < pokemon.move.length - 1 ? ", " : ""}
+                </span>
+              );
+            })}
           </div>
-        
         </div>
       </div>
-      
+
       <div className="btns">
-        
-          <div className="left">
-            
-            <div className="flexBox">
-              <button className="wid30 bg1"></button>
-              <button className="wid70 bg2"></button>
-            </div>
-            
-            <div className="flexBox">
-              <button className="wid70 bg3"></button>
-              <button className="wid30 bg2"></button>
-            </div>
-            
-            <div className="flexBox">
-              <button className="wid100 bg1"></button>
-            </div>
-            
-            <div className="flexBox mHide">
-              <button className="wid30 bg2"></button>
-              <button className="wid70 bg3"></button>
-            </div>
-            
+        <div className="left">
+          <div className="flexBox">
+            <button className="wid30 bg1"></button>
+            <button className="wid70 bg2"></button>
           </div>
-          
-          <div className="right">
-              <div className="gridBox">
-                <button className="top" onClick={() => moveBottom()}></button>
-                <button className="left"></button>
-                <button className="center"></button>
-                <button className="right"></button>
-                <button className="bottom"onClick={() => moveTop()}></button>
-              </div>
+
+          <div className="flexBox">
+            <button className="wid70 bg3"></button>
+            <button className="wid30 bg2"></button>
           </div>
-          
+
+          <div className="flexBox">
+            <button className="wid100 bg1"></button>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="gridBox">
+            <button className="top" onClick={() => moveTop()}></button>
+            <button className="left"></button>
+            <button className="center"></button>
+            <button className="right"></button>
+            <button className="bottom" onClick={() => moveBottom()}></button>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default React.memo(PokemonDetail)
+export default React.memo(PokemonDetail);
